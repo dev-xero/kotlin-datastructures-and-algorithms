2b5e80773f28564d4ed474cd51d71b6c8dc9f3c8
Xero kun <elo48858@gmail.com>
2023-06-22 12:18:21 +0100

    `TEST` Tested non-empty linked list returns correctly

--- src/test/kotlin/testdatastructures/TestLinkedList.kt
+++ src/test/kotlin/testdatastructures/TestLinkedList.kt
@@ -1,6 +1,7 @@
 package testdatastructures
 
 import datastructures.LinkedList
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -16,5 +17,13 @@ class TestLinkedList {
     @Test
     fun testLinkedListEmptyReturnsTrue() {
         assertTrue(linkedList.isEmpty())
+    }
+
+    @Test
+    fun testNonEmptyLinkedListReturnsFalse() {
+        linkedList.addToTail(1)
+        linkedList.addToTail(2)
+
+        assertFalse(linkedList.isEmpty())
     }
 }
